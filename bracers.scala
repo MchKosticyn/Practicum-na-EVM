4459e01fec66fee7e185ba30ebc2e96385b7fd06
Misha Kosticyn <mishakosticyn@yandex.ru>
2016-11-25 23:38:13 +0400

    Цикл -> рекурсия

--- bracers.scala
+++ bracers.scala
@@ -2,21 +2,24 @@
   * Created by Asus on 21.11.2016.
   */
 object bracers {
-  def correct(s:List[Char]) ={
-    var l = 0
-    for (e <- s) {
-      if (e == '(')
-        l += 1
-      if (e == ')')
-        l -= 1
+  def correct(s:List[Char], n : Int = 0):Boolean ={
+    //var f : Boolean = false
+    if (s.isEmpty)
+      {
+        if (n==0)true
+        else false
+      }
+    else {
+      if (n>=0) {
+        if (s.head == '(')
+          correct(s.drop(1), n + 1)
+        else correct(s.drop(1), n - 1)
+      }
+        else false
     }
-    if (l==0)
-      true
-    else
-      false
   }
   def main(args: Array[String]): Unit = {
-   if (correct(List('(',')')))
+   if (correct(readLine("Write a string: ").toList,0))
      print("""Correct!""")
    else  print("""Not correct!""")
   }
