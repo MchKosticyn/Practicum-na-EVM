e9ee6669ab5538c25a44b4b58d6d9f1d7f94e6a3
Misha Kosticyn <mishakosticyn@yandex.ru>
2016-12-27 01:39:15 +0400

    Fineal checker

--- CheckLog.scala
+++ CheckLog.scala
@@ -6,72 +6,63 @@ package BarberShopP
 import scala.collection.mutable
 import scala.io.Source
 
-class CheckLog(visitors : Int, barbers : Int, sizeOfSofa : Int) {
+object CheckLog {
 
-  var barbersState = mutable.Map[Long, Long]()
-  var atSofaNow = 0
+  var AtSofaNow = 0
+  var Barbers: Array[Int] = Array()
+  val getInfo: List[String] = Source.fromFile("log.txt").getLines().toList
 
-  def getInfo() : List[String] = {
-    Source.fromFile("log.txt").getLines().toList
-  }
-
-  def parseStr(str : String) : Unit = {
-    val info = str.split(' ')
-    val actor = info(0)
-    val id = info(1).toLong
-
-    actor match {
-      case "Visitor" =>
-        if (info(2) == "at")
-          atSofaNow += 1
-
-      case "Barber"  =>
-        if (info(2) == "take") {
-          if (barbersState.contains(id)) {
-            if (barbersState.apply(id) == -1) {
-              barbersState(id) = info(3).toLong
-              atSofaNow -= 1
-            }
-            else {
-              println("Double visitors")
-              System.exit(0)
-            }
-          }
-          else {
-            barbersState update (id, info(3).toLong)
-            atSofaNow -= 1
-          }
-        }
-        else {
-          if (barbersState.apply(id) != -1) {
-            barbersState(id) = -1
-          }
-          else {
-            println("Free barber done work")
-            System.exit(0)
-          }
-        }
-      case _ => {
-        println("Unknown log")
-        System.exit(0)
+  def parseStr(str: String, visitors: Int, barbers: Int, size: Int): (Boolean, String) = {
+    val A = str.split(' ')
+    var l = 0
+    if (A.contains("Enq")) l = 1
+    else if (A.contains("deq")) l = 2
+    else if (A.contains("done")) l = 3
+    else if (A.contains("back")) l = 4
+    l match {
+      case 0 =>
+        (false, "Wrong log")
+      case 1 => {
+        val ID = A(1)
+        AtSofaNow += 1
+        if (AtSofaNow > size)
+          (false, "Sofa overflow!")
+        else (true, "")
       }
+      case 2 => {
+        val ID = A(0).toInt
+        val VID = A(2).toInt
+        AtSofaNow -= 1
+        Barbers(ID - 1) += 1
+        if (Barbers(ID - 1) > 1)
+          (false, "Chair overflow!")
+        else (true, "")
+      }
+      case 3 => {
+        val ID = A(0).toInt
+        val VID = A(2).toInt
+        Barbers(ID - 1) -= 1
+        if (Barbers(ID - 1) < 0)
+          (false, "Not done yet!")
+        else (true, "")
+      }
+      case 4 =>
+        if (AtSofaNow < size)
+          (false, "Wrong direction!")
+        else (true, "")
     }
-
-    if (atSofaNow > sizeOfSofa) {
-      println("Sofa overflow")
-      System.exit(0)
-    }
-
-    if (atSofaNow < 0) {
-      println("Taking from empty sofa")
-      System.exit(0)
-    }
-
   }
 
-  def main(): Unit = {
-    getInfo().map(x => parseStr(x))
-    println("Errors is log:")
+  def StartCheck(visitors: Int, barbers: Int, SizeOfSofa: Int): Unit = {
+    Barbers = new Array(barbers)
+    Barbers = Barbers.map(x => 0)
+    println("Log checker started...")
+    val BList = getInfo.map(parseStr(_, visitors, barbers, SizeOfSofa)).zipWithIndex
+    BList.foreach(x =>
+      if (x._1._1 == false) println("Error in line " + (x._2+1) + " ( " + x._1._2 + " )" )
+    )
+    if (!BList.map(_._1._1).contains(false))
+      println("Clear!")
   }
 
 }
